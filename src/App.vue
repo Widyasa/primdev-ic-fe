0b5104a6992141942fca46a8cd877013b897fc6b
widyasa <widyarusmananda15@gmail.com>
2026-03-07 11:25:29 +0800

    feat: add HeroSection, MainFooter, and MainNavbar components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
+import HeroSection from './components/HeroSection.vue';
+import MainFooter from './components/MainFooter.vue';
+import MainNavbar from './components/MainNavbar.vue';
+
+
 </script>
 
 <template>
-  <main class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="text-center p-8 bg-white rounded-xl shadow-lg">
-      <p class="text-gray-600">
-	      Hello World
-        Setup Vue.js dan Tailwind CSS v4 berhasil!
-      </p>
-    </div>
-  </main>
+  <MainNavbar />
+  <HeroSection />
+  <MainFooter />
 </template>
